48108051b3a7d30b605572bf4dec40cacfa69353
stefanm95 <85728264+stefanm95@users.noreply.github.com>
2022-07-20 11:08:52 +0300

    first1.01

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number🥳';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
 let highScore = 0;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -47,40 +40,15 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      //   document.querySelector('.message').textContent = '💣 You lost the game!';
       displayMessage(' You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
-
-  //when guess is too high
-  //    else if (guess > secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent = '📈 Number is too high!';
-  //       score--;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = '💣 You lost the game!';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-
-  //     //when guess is too low
-  //   } else if (guess < secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent = '📉 Number is too low!';
-  //       score--;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = '💣 You lost the game!';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  //   document.querySelector('.message').textContent = 'Start guessing...!';
   displayMessage('Start guessing...!');
   document.querySelector('.guess').value = '';
   document.querySelector('.score').textContent = score;
@@ -88,6 +56,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-//refactoring the code to improve and eliminate duplciate code
-
-// first ui component (modal window)!
